0988630b0f3f73dc95ddf03dbf8f33f929d1930e
Jose Alan <alan123art@gmail.com>
2018-03-17 16:29:11 -0300

    Part1 - Criando Adpter e fazendo bind das informacoes.

--- Part1/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ Part1/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -2,8 +2,14 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.dao.PacoteDAO;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -11,5 +17,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaPacotes = findViewById(R.id.lista_pacote_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        
+        listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
